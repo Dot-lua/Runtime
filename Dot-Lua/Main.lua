84ab005670e582d4772bd026f7083dad5b8afa51
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-06-01 17:52:12 +0200

    Update Main.lua

--- Dot-Lua/Main.lua
+++ Dot-Lua/Main.lua
@@ -11,15 +11,16 @@ coroutine.wrap(function ()
 
     _G.GitLib = require("Github-Lib")
     _G.Logger = require("Logger")
-    --_G.BuildHelper = require("BuildHelper")
+    _G.ProcessHelper = require("ProcessHelper")
     _G.FS = require("fs")
     _G.Json = require("json")
-    --_G.Watch = require("discordia").Stopwatch()
-    --Watch:start()
+    _G.Watch = require("discordia").Stopwatch()
+    Watch:start()
+    
 
     if not GitLib.IsLatest() then
         Logger.Warn("You are running an older version of the Dot-Lua Runtime!")
-        Logger.Warn("Update it! - View how to do so at http://Dotter.cubicinc.ga/reinstall")
+        Logger.Warn("Update it! - View how to do so at http://Dotter.cubicinc.ga/update")
         Logger.Warn("Your version: " .. GitLib.GetVersion() .. " Remote version: " .. GitLib.GetRemoteVersion())
     else
         Logger.Info("Starting Runtime!")
@@ -29,18 +30,21 @@ coroutine.wrap(function ()
     print()
     
     _G.Tasks = {
-        run = require("./Tasks/Run.lua")
+        run = require("./Tasks/Run.lua"),
+        help = require("./Tasks/Help.lua")
     }
     
     if not Args[1] then
-        Tasks.help(Args)
+        Tasks.help.Execute(Args)
     else
         local Command = string.lower(Args[1])
 
+        table.remove(Args, 1)
+
         if Tasks[Command] then
-            Tasks[Command]()
+            Tasks[Command].Execute(Args)
         else
-            BuildHelper.Fail("Command '" .. Command .. "' Does not exist!")
+            ProcessHelper.Fail("Command '" .. Command .. "' does not exist!")
         end
 
     end
