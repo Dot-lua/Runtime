b3e5f67aafba9ce4eea128f3ec2e1b69b6fa4e11
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-06-02 13:05:48 +0200

    help update

--- Dot-Lua/Tasks/Help.lua
+++ Dot-Lua/Tasks/Help.lua
@@ -1,15 +1,34 @@
 return {
+    Arguments = {
+        "[Command]"
+    },
     Execute = function (Args)
     
-        Logger.Error("You gave no or a unknown command to the runtime!")
-        Logger.Error("Usage: `./Dot-Lua [Task] [Options]`")
-        Logger.Error("You can use the following commands:")
+        if Args[1] then
 
-        for i, v in pairs(Tasks) do
-            Logger.Error("- " .. i)
+            local LowerCommand = string.lower(Args[1])
+
+            if not Tasks[LowerCommand] then 
+                ProcessHelper.Fail("The command '" .. Args[1] .. "' does not exist!")
+                return
+            end
+
+            Logger.Info("Usage for: '" .. Args[1] .. "'")
+            Logger.Info("'Dot-Lua " .. Args[1] .. " " .. table.concat(Tasks[LowerCommand].Arguments, " ") .. "'")
+
+        else
+
+            Logger.Error("You gave no or a unknown command to the runtime!")
+            Logger.Error("Usage: `./Dot-Lua [Task] [Options]`")
+            Logger.Error("You can use the following commands:")
+
+            for i, v in pairs(Tasks) do
+                Logger.Error("- " .. i .. " " .. table.concat(v.Arguments, " "))
+            end
+
+            ProcessHelper.Fail()
+
         end
-
-        ProcessHelper.Fail()
 
     end
 }
