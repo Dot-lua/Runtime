83a0a30790d3fa3ccdeca58aba55ef09ca745a27
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-06-02 13:05:54 +0200

    work on run

--- Dot-Lua/Tasks/Run.lua
+++ Dot-Lua/Tasks/Run.lua
@@ -1,5 +1,23 @@
 return {
+    Arguments = {
+        "[File]",
+        "--debugmode=[Mode: true, false]"
+    },
     Execute = function(Args)
-    
+        local Path = require("Path")
+
+        if not Args[1] then
+            ProcessHelper.Fail("File not found!")
+            return
+        end
+
+        local FilePos = Path.normalize(Args[1])
+
+        Logger.Info("Loading file " .. FilePos)
+
+        if not FilePos then
+            ProcessHelper.Fail("File " .. Args[1] .. " not found!")
+            return
+        end
     end
 }
