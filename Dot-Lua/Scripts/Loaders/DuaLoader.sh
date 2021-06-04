01caf81077200003297250d3869da30de67f718a
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-06-04 14:22:07 +0200

    Update DuaLoader.sh

--- Dot-Lua/Scripts/Loaders/DuaLoader.sh
+++ Dot-Lua/Scripts/Loaders/DuaLoader.sh
@@ -1,2 +1,11 @@
-$ProcessId = $1
-$Location = $2
+ProcessId=$1
+Location=$2
+FileName=$3
+
+echo $ProcessId
+echo $Location
+echo $FileName
+
+echo Working
+cp "$Location" "./Cache/Processes/$ProcessId/Archives/"
+unzip ./Cache/Processes/$ProcessId/Archives/$FileName -d ./Cache/Processes/$ProcessId/UnpackCache/
