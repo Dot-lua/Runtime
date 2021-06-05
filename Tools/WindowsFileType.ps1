1b5c7c5f5df7dd42c411aabd846619b5857357bd
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-06-05 19:51:32 +0200

    Update WindowsFileType.ps1

--- Tools/WindowsFileType.ps1
+++ Tools/WindowsFileType.ps1
@@ -4,9 +4,17 @@ Write-Output "assoc"
 cmd /c assoc .dua=DuaArchive
 Write-Output "ftype"
 
-$Path = Resolve-Path -Path "$PSScriptRoot/../Dot-Lua/Launchers/WindowsFileLauncher.bat"
+$Launcher = Resolve-Path -Path "$PSScriptRoot/../Dot-Lua/Launchers/WindowsFileLauncher.bat"
+$Icon = Resolve-Path -Path "$PSScriptRoot/../Dot-Lua/Assets/Logo.png"
+Write-Output $Launcher
 
-cmd /c ftype DuaArchive=$Path %1 %*
+cmd /c ftype DuaArchive=$Launcher %%1 %%*
 Write-Output done
 Write-Output ""
+
+#cmd /c REG ADD HKEY_CLASSES_ROOT\DuaArchive\DefaultIcon /v $Icon,1 /t REG_EXPAND_SZ /d $Icon,1
+
+cmd /c ftype DuaArchive
+cmd /c reg query HKCR\DuaArchive\SHELL\OPEN\COMMAND /ve
+
 pause
