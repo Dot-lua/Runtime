812d87904574f8715f4f54d48da3a5daf8cb91d6
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-06-05 19:51:38 +0200

    run task

--- Dot-Lua/Tasks/Run.lua
+++ Dot-Lua/Tasks/Run.lua
@@ -1,3 +1,5 @@
+local Logger = require("Logger")
+
 return {
     Arguments = {
         "[File]",
@@ -5,36 +7,19 @@ return {
     },
     Execute = function(Args)
         local Path = require("Path")
+        local Split = require("Split")
 
         if not Args[1] then
             ProcessHelper.Fail("File not found!")
             return
         end
 
-        local FilePos = Path.resolve(Args[1])
-        local FileBase = Path.basename(FilePos)
-        local Extension = Path.extname(FileBase)
-        local Exists = FS.existsSync(FilePos)
-        
+        _G.ProcessId = require("RandomString")(10)
 
-        local CorrectExtension = Extension == ".dua"
-
-        Logger.Info("Trying to load file " .. FilePos)
-        Logger.Info("File name is: " .. FileBase)
-        Logger.Info("Is Correct Extension: " .. tostring(CorrectExtension))
-        Logger.Info("Exstension: '" .. (Extension or "") .. "'")
-        Logger.Info("Exists: " .. tostring(CorrectExtension))
-        
-        if not Exists or not Extension then
-            ProcessHelper.Fail("'" .. FileBase .. "' is not a valid DUA archive!")
-            return
-        end
-
-        ProcessId = require("RandomString")(10)
-
-        print()
+        --print()
 
         Logger.Info("Starting process with id: " .. ProcessId)
+        _G.ProcessPath = RuntimePath .. "Cache/Processes/" .. ProcessId .. "/"
 
         FS.mkdirSync("./Cache/Processes/")
         FS.mkdirSync("./Cache/Processes/" .. ProcessId .. "/")
@@ -43,48 +28,49 @@ return {
         FS.mkdirSync("./Cache/Processes/" .. ProcessId .. "/Resources/")
         FS.mkdirSync("./Cache/Processes/" .. ProcessId .. "/UnpackCache/")
 
-        print()
+        --print()
+
+        _G.LoadedPackages = {}
         
-        local CommandWindows = "PowerShell -NoProfile -ExecutionPolicy unrestricted -File ./Scripts/Loaders/DuaLoader.ps1 " .. ProcessId .. " " .. FilePos .. " " .. FileBase
-        local CommandMac = "sh ./Scripts/Loaders/DuaLoader.sh " .. ProcessId .. " " .. FilePos .. " " .. FileBase
-
-        local Handle
-
-        if WorkingOS == "Windows" then
-            Handle = io.popen(CommandWindows)
-        elseif WorkingOS == "Mac" then
-            --FS.chmodSync(CommandMac, 744)
-            Handle = io.popen(CommandMac)
-        end
-
-        for Line in Handle:lines() do
-            Logger.Info(Line)
-        end
-
-        FS.renameSync(
-            "./Cache/Processes/" .. ProcessId .. "/UnpackCache/resources/",
-            "./Cache/Processes/" .. ProcessId .. "/Resources/"
-        )
-
-        FS.renameSync(
-            "./Cache/Processes/" .. ProcessId .. "/UnpackCache/package.info.lua",
-            "./Cache/Processes/" .. ProcessId .. "/Running/package.info.lua"
-        )
-
-        local DirFiles = FS.readdirSync("./Cache/Processes/" .. ProcessId .. "/UnpackCache/")
-
-        for i, v in pairs(DirFiles) do
-            FS.renameSync(
-                "./Cache/Processes/" .. ProcessId .. "/UnpackCache/" .. v,
-                "./Cache/Processes/" .. ProcessId .. "/Running/" .. v
-            )
-        end
-
-        local ProcessInfo = require("../Cache/Processes/" .. ProcessId .. "/Running/package.info.lua")
-
         _G.Import = require("Runtime/Import")
+        _G.LoadPackage = require("Runtime/LoadPackage")
+        _G.LoadInternal = require("Runtime/LoadInternal")
 
-        Import(ProcessInfo.Entrypoints.Main)
+        LoadPackage(Args[1], false, true)
+
+        
+        local MainEntry = Import(ProcessInfo.Entrypoints.Main)
+
+        if not MainEntry then
+            ProcessHelper.Fail("The main entry of " .. ProcessInfo.ID .. " '" .. ProcessInfo.Entrypoints.Main .. "' was not found!")
+            process:exit(1)
+        end
+
+        local EntryWorked, EntryError = pcall(function()
+            MainEntry.OnInitialize()
+        end)
+
+        if not EntryWorked then
+
+            local ParseStepOne = Split(EntryError, "\\")
+
+            table.remove(ParseStepOne, 1)
+            table.remove(ParseStepOne, 1)
+            table.remove(ParseStepOne, 1)
+
+            local ParseStepTwo = ParseStepOne[#ParseStepOne]    
+            table.remove(ParseStepOne, #ParseStepOne)
+            local TwoParsed = Split(ParseStepTwo, ":")
+
+            print()
+            Logger.Error("Error at: '" .. table.concat(ParseStepOne, ".") .. "." .. TwoParsed[1] .. "' line: " .. TwoParsed[2])
+            ProcessHelper.Fail(string.sub(TwoParsed[3], 2))
+
+            process:exit(1)
+        else
+            ProcessHelper.Complete()
+        end
+        
 
 
 
