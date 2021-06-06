1280b192c9cbe13f99f4fb8cbe7e68b6a8c8333e
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-06-06 20:08:27 +0200

    Update Windows.ps1

--- Dot-Lua/Launchers/Windows.ps1
+++ Dot-Lua/Launchers/Windows.ps1
@@ -1,6 +1,5 @@
-$Path = Resolve-Path -Path "$PSScriptRoot/.."
+$Path = Resolve-Path -Path "$PSScriptRoot/../"
 
-Set-Alias -Name Dot-Lua -Value "$PSScriptRoot/Windows.ps1" -Description "Dot-Lua Runtime"
+#Set-Alias -Name Dot-Lua -Value "$PSScriptRoot/Windows.ps1" -Description "Dot-Lua Runtime" -Option AllScope
 
-Powershell -Command "$Path/Envoirment/Luvit/luvit $Path/Main.lua Windows $args"
-
+Powershell -NoProfile -ExecutionPolicy unrestricted -Command "$Path/Envoirment/Luvit/luvit $Path/Main.lua Windows $args"
